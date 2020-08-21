5cf2fb63c9c3d59dc32e40622584e7fa89bbd35e
Diego Gomez <dagomez2530@gmail.com>
2020-08-21 02:57:05 +0000

    Adding file

--- 0x00-challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
+++ 0x00-challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -39,12 +39,13 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	}
 	else
 	{
-		(*head)->prev->prev = (*head)->prev;
 		if ((*head)->next)
 		{
 			(*head)->prev->next = (*head)->next;
 			(*head)->next->prev = (*head)->prev;
 		}
+		else
+			(*head)->prev->next = NULL;
 		free(*head);
 		*head = saved_head;
 	}
